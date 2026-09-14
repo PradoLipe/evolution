fcaad583578cf4ed28b5d9dc3a7a94789a7e3250
PradoLipe <kardlipe@gmail.com>
2026-09-14 07:28:49 -0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
     // ============================================
 
         // Versao do App
-    window.EVOLUTION_APP_VERSION = 'V5.95';
+    window.EVOLUTION_APP_VERSION = 'V5.94';
 
 // Configuracao Firebase
     const firebaseConfig = {
